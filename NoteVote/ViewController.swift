62661252dc98ac684508b56749e2ab9f6c790f61
Dustin Jones <jonesy716151@Dustins-MacBook-Pro.local>
2017-07-30 00:58:17 -0500

    Initial Commit

---
+++ NoteVote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoteVote
+//
+//  Created by Dustin Jones on 7/30/17.
+//  Copyright © 2017 Dustin Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
